d69b7ee20e24f55773fde8ae8b28248b743dc7b3
gk <grzegorz.korcyl@gmail.com>
2014-05-05 16:13:04 +0200

    update

--- local_link_dummy.vhd
+++ local_link_dummy.vhd
@@ -101,7 +101,7 @@ begin
 			end if;
 		
 		when GENERATE_DATA =>
-			if (data_ctr = data_stop_val) then
+			if (data_ctr = data_stop_val(15 downto 0)) then
 				link_next_state <= TIMEOUT;
 			else
 				link_next_state <= GENERATE_DATA;
